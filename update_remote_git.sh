6eac9c7c75fe39d0b9f220edfa5e5e365a583120
Steven <sbirch@localhost.localdomain>
2018-09-15 21:46:01 -0500

    autocommit

--- update_remote_git.sh
+++ update_remote_git.sh
@@ -4,11 +4,6 @@
 # updates remote github repository to match local
 ###########################################################################
 
-git fetch --all
-git checkout master
-git config merge.defaultToUpstream true
-git merge
-git add .
 
 if [[ -z $1 ]]
 then
@@ -16,6 +11,13 @@ then
 else
     commit_message="$1"
 fi
+
+git fetch --all
+git checkout master
+git config merge.defaultToUpstream true
+git merge
+
+git add --all .
 
 git commit -m "${commit_message}"
 git push origin master
